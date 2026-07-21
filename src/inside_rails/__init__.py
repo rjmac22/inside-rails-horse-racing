4b8abbf50c565f0bdab80318c897d5dc34cf97e8
rjmac22 <rjmac@live.co.uk>
2026-07-21 17:32:53 +0100

    Export source profiling utilities

--- src/inside_rails/__init__.py
+++ src/inside_rails/__init__.py
@@ -1,1 +1,19 @@
 """Reusable code for the Inside Rails horse-racing database project."""
+
+from .source_sqlite import (
+    HEADER_ROWID,
+    PROVISIONAL_RACE_COLUMNS,
+    PROVISIONAL_RUNNER_COLUMNS,
+    connect_read_only,
+    profile_source_database,
+    quote_identifier,
+)
+
+__all__ = [
+    "HEADER_ROWID",
+    "PROVISIONAL_RACE_COLUMNS",
+    "PROVISIONAL_RUNNER_COLUMNS",
+    "connect_read_only",
+    "profile_source_database",
+    "quote_identifier",
+]
