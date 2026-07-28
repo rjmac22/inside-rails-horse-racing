6ce961664c0487e26d6809bcebad16af7e863ba3
rjmac22 <rjmac@live.co.uk>
2026-07-28 15:32:07 +0100

    Export prize money parser

--- src/inside_rails/__init__.py
+++ src/inside_rails/__init__.py
@@ -1,5 +1,6 @@
 """Reusable code for the Inside Rails horse-racing database project."""
 
+from .prize_money import PrizeMoneyResult, parse_prize_money
 from .source_sqlite import (
     HEADER_ROWID,
     PROVISIONAL_RACE_COLUMNS,
@@ -13,7 +14,9 @@ __all__ = [
     "HEADER_ROWID",
     "PROVISIONAL_RACE_COLUMNS",
     "PROVISIONAL_RUNNER_COLUMNS",
+    "PrizeMoneyResult",
     "connect_read_only",
+    "parse_prize_money",
     "profile_source_database",
     "quote_identifier",
 ]
