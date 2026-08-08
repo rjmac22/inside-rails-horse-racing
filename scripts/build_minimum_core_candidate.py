f8470ebf7ba15ae6c9fc8a9100ff1cf01d8d35d4
rjmac22 <rjmac@live.co.uk>
2026-08-08 12:11:38 +0100

    Rename minimum core candidate output to Inside Rails

--- scripts/build_minimum_core_candidate.py
+++ scripts/build_minimum_core_candidate.py
@@ -33,7 +33,7 @@ DEFAULT_RAW_MIRROR = (
     / "processed"
     / "database"
     / "candidates"
-    / "raceform_v1_raw_mirror_candidate.sqlite3"
+    / "inside_rails_v1_raw_mirror_candidate.sqlite3"
 )
 DEFAULT_OUTPUT = (
     ROOT
@@ -41,7 +41,7 @@ DEFAULT_OUTPUT = (
     / "processed"
     / "database"
     / "candidates"
-    / "raceform_v1_minimum_core_candidate.sqlite3"
+    / "inside_rails_v1_candidate.sqlite3"
 )
 
 
